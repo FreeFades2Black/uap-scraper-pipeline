973c4a3a53072ef094c7d11a685581a0db77659d
Free Hall <whall4.wh@gmail.com>
2026-07-24 10:10:14 -0500

    Update Databricks configuration for Git sync workflow

--- terraform/databricks.tf
+++ terraform/databricks.tf
@@ -1,3 +1,22 @@
-resource "databricks_secret_scope" "scraper_secrets" {
-  name = "scraper_credentials"
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+  }
+}
+
+provider "databricks" {}
+
+resource "databricks_job" "process_anomalies" {
+  name = "Process-UAP-Historical-Anomalies"
+
+  task {
+    task_key            = "process_gcs_raw_data"
+    existing_cluster_id = "your-manual-cluster-id-here"
+
+    notebook_task {
+      notebook_path = "/Workspace/Shared/process_historical_anomalies"
+    }
+  }
 }
