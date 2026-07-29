7b65b429761de22555039520b7b1fc8ee8a47228
Free Hall <whall4.wh@gmail.com>
2026-07-29 13:09:20 -0500

    Clean up provider blocks in databricks.tf

--- terraform/databricks.tf
+++ terraform/databricks.tf
@@ -1,22 +1,2 @@
-terraform {
-  required_providers {
-    databricks = {
-      source = "databricks/databricks"
-    }
-  }
-}
+# Databricks resources placeholder
 
-provider "databricks" {}
-
-resource "databricks_job" "process_anomalies" {
-  name = "Process-UAP-Historical-Anomalies"
-
-  task {
-    task_key            = "process_gcs_raw_data"
-    existing_cluster_id = "your-manual-cluster-id-here"
-
-    notebook_task {
-      notebook_path = "/Workspace/Shared/process_historical_anomalies"
-    }
-  }
-}
